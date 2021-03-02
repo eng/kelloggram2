929bcfeefb5a820c263ae3b333efaffb86270947
Brian Eng <brian@brianeng.com>
2021-03-01 19:30:59 -0600

    config

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyDKx39B07UGPUMhfbNfhO-7aQyen_PgsuI",
+  authDomain: "kiei-451-winter-2021.firebaseapp.com",
+  projectId: "kiei-451-winter-2021",
+  storageBucket: "kiei-451-winter-2021.appspot.com",
+  messagingSenderId: "751368185998",
+  appId: "1:751368185998:web:437cc4e490fd432789c1df"
+}
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
